fec6761c99a331d44d116fe1701c179b4ebeb2ca
Darren Torpey <dct@punchbowlsoftware.com>
2010-08-21 17:43:02 -0400

    More bundle usage

--- resources/bundle.js
+++ resources/bundle.js
@@ -8,5 +8,46 @@
 		['enemy_sprite',    'resources/enemy_sprite.png'],
 		['player_bullet',   'resources/bullet.png'],
 		['sprites',         'resources/explode.png']
-	]
+	],
+
+  addFont: [
+    { id: 'small', image: 'font', firstletter: ' ', tileh: 8, tilew: 8, tilerow: 255, gapx: 0, gapy: 0 }
+  ],
+
+  addTiles:[
+   {id:"explosion",image:"sprites",tileh:32,tilew:32,tilerow:4,gapx:0,gapy:0},
+   {
+    id:      'player_tiles',
+    image:   'player_sprite',
+    tileh:   16,
+    tilew:   16,
+    tilerow: 19,
+    gapx:    0,
+    gapy:    0
+  }, {
+    id:      'map_pieces',
+    image:   'map_spritesheet',
+    tileh:   16,
+    tilew:   16,
+    tilerow: 1,
+    gapx:    0,
+    gapy:    0
+  }, { // New for Part 5, adding the enemy sprite
+    id:      'enemy_tiles',
+    image:   'enemy_sprite',
+    tileh:   16,
+    tilew:   16,
+    tilerow: 1,
+    gapx:    0,
+    gapy:    0
+  }, {
+    id:       'player_shot',
+    image:    'player_bullet',
+    tileh:    4,
+    tilew:    4,
+    tilerow:  2,
+    gapx:     0,
+    gapy:     0
+  }
+  ]
 }
