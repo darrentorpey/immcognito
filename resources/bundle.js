4623c8cc98b3253e01447b94ca56db97f5a8d5ce
Darren Torpey <dct@punchbowlsoftware.com>
2010-08-21 17:38:36 -0400

    Using the new BUNDLE feature -- wooo

---
+++ resources/bundle.js
@@ -0,0 +1,12 @@
+{
+	// Graphic resources.
+	addImage: [	
+		['font',            'resources/font.png'],
+		['logo',            'resources/logo.png'],
+		['player_sprite',   'resources/player_sprite.png'],
+		['map_spritesheet', 'resources/map_pieces.png'],
+		['enemy_sprite',    'resources/enemy_sprite.png'],
+		['player_bullet',   'resources/bullet.png'],
+		['sprites',         'resources/explode.png']
+	]
+}
